4d11e08fdc6d911beb6bafe2edbf54abd1bb8b31
Raul Felix Carrizo <raulfelixcarrizo@gmai.com>
2019-08-29 13:16:59 +1000

    Updates

---
+++ precache-manifest.3b4f0307c0f5960b545cb4d42f0de4d2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4ad749b8c6b57425e480648d26e6d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bef385f2fca81b496b1",
+    "url": "/static/css/main.11309fb0.chunk.css"
+  },
+  {
+    "revision": "addc2dbf080a0c786c45",
+    "url": "/static/js/2.f3f431b8.chunk.js"
+  },
+  {
+    "revision": "8bef385f2fca81b496b1",
+    "url": "/static/js/main.21031f12.chunk.js"
+  },
+  {
+    "revision": "a9f0b6fe4b677de3152e",
+    "url": "/static/js/runtime~main.9e72ac00.js"
+  },
+  {
+    "revision": "0c481a968d75ae81a347e6929b5d7931",
+    "url": "/static/media/CircularStd-Black.0c481a96.eot"
+  },
+  {
+    "revision": "9def419b2a0863c27eed34764e6ff852",
+    "url": "/static/media/CircularStd-Black.9def419b.woff"
+  },
+  {
+    "revision": "eab2d208aefd02d27d58d3098c2b5ad8",
+    "url": "/static/media/CircularStd-Black.eab2d208.ttf"
+  },
+  {
+    "revision": "762907ae2f238fa078fff8cb1484e484",
+    "url": "/static/media/MarvinVisions-Bold.762907ae.woff"
+  },
+  {
+    "revision": "e1817e354676d5d9fddae77fe641e130",
+    "url": "/static/media/MarvinVisions-Bold.e1817e35.eot"
+  },
+  {
+    "revision": "b7a5ee3a0dfe2a0529b7c644d762d5b5",
+    "url": "/static/media/brain.b7a5ee3a.svg"
+  },
+  {
+    "revision": "463a8a8d434687755851f4bd2fcbfc31",
+    "url": "/static/media/compass.463a8a8d.svg"
+  },
+  {
+    "revision": "04e80357f0bf1cbc4bfb5537d8edcf56",
+    "url": "/static/media/icomoon.04e80357.eot"
+  },
+  {
+    "revision": "10749cbd030fb40e504eb2ba33a40cb2",
+    "url": "/static/media/icomoon.10749cbd.ttf"
+  },
+  {
+    "revision": "47c947fd32b852a058a9bfd520953e10",
+    "url": "/static/media/icomoon.47c947fd.woff"
+  },
+  {
+    "revision": "a5a188eaaef229fa399cb28551267061",
+    "url": "/static/media/icomoon.a5a188ea.svg"
+  },
+  {
+    "revision": "036ba564d71287f9687fd650a21077a3",
+    "url": "/static/media/raul_es.036ba564.mp3"
+  }
+]);
