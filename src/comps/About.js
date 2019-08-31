2cc11d59f6e249aad2672a6183c34ac09ae60698
Raul Felix Carrizo <raul.carrizo@shippit.com>
2019-08-31 14:47:09 +1000

    Better responsiveness

--- src/comps/About.js
+++ src/comps/About.js
@@ -5,15 +5,21 @@ const About = () => (
   <section className="content-row centered yellow">
     <div className="f-grid f-row">
       <div className="f-1">
-        <div className="section-header">
-          <img className="section-icon" src={brain} alt="The brain" />
-          <h2 className="section-title">A few things about me</h2>
+        <div className="section-header section-header-inline mb-4">
+          <img className="section-icon section-icon--brain" src={brain} alt="The brain" />
+          <h2 className="section-title">A bit about me</h2>
         </div>
+      </div>
+    </div>
+    <div className="f-grid f-row">
+      <div className="f-1-2 bp-m-1 bp-s-1">
         <p>Football is my first love.</p>
         <p>I speak Spanish and Italian.</p>
         <p>I am a good listener... talk to me.</p>
         <p>Peroni is my beer.</p>
-        <p>Instead of a list of places I worked at I think knowing who you are dealing with is more important… Check out <a href="https://www.linkedin.com/in/ra%C3%BAl-felix-carrizo-70716439/" target="_blank" rel="noopener noreferrer">linkedIn</a> if you do want to see my professional history.</p>
+      </div>
+      <div className="f-1-2 bp-m-1 bp-s-1">
+      <p>You didn't find my work history here because I think knowing who you are dealing with is important… Check out <a href="https://www.linkedin.com/in/ra%C3%BAl-felix-carrizo-70716439/" target="_blank" rel="noopener noreferrer">linkedIn</a> if you do want to see it.</p>
       </div>
     </div>
   </section>
