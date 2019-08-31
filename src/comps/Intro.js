9f16876c3d1125100696fdf0883af61d9cd7f183
Raul Felix Carrizo <raul.carrizo@shippit.com>
2019-08-31 14:51:43 +1000

    Remove shit

--- src/comps/Intro.js
+++ src/comps/Intro.js
@@ -6,8 +6,7 @@ const Intro = () => (
       <div className="f-1">
         <p>I specialise in <em>Front end</em> architecture and development. My primary focus is on web and mobile application development, user experience 
           and rapid prototyping.</p> 
-        <p>I enjoy technical challenges and write a lot of <em>HTML</em>, <em>CSS</em> and <em>Javascript</em> and have plenty experience with
-          the cool shit like <em>React</em>, <em>React Native</em> and <em>Vue.js</em>.
+        <p>I enjoy technical challenges and write a lot of <em>HTML</em>, <em>CSS</em> and <em>Javascript</em> and have plenty experience with <em>React</em>, <em>React Native</em> and <em>Vue.js</em>.
         </p>
       </div>
     </div>
