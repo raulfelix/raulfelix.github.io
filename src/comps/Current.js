7a88c791544ce7c8340b71fa570c74939f18bfda
raul <raul.carrizo@inquisitive.com>
2020-02-09 20:44:27 +1100

    Update where I work

--- src/comps/Current.js
+++ src/comps/Current.js
@@ -5,13 +5,13 @@ const Current = () => (
   <section className="content-row blue" id="current">
     <div className="f-grid f-row">
       <div className="f-1">
-          <p className="section-title">Currently...</p> 
-          <p>I am making good ship at <a href="https://www.shippit.com/" target="_blank" rel="noopener noreferrer">Shippit</a> using my knowledge and experience in UX, design and development to build complex and engaging applications which solve real world problems for our customers.</p>
-          <p>I like to collaborate in my spare time, to chat you can reach me at <a href="mailto:raulfelixcarrizo@gmail.com">raulfelixcarrizo@gmail.com</a></p>
-          <NextButton target="engineer" color="#4A4A4A" />
-        </div>
+        <p className="section-title">Currently...</p> 
+        <p>I am working away at <a href="https://www.inquisitive.com/" target="_blank" rel="noopener noreferrer">Inquisitive</a> where we are doing a world of good for teachers and students everywhere.</p>
+        <p>I like to collaborate in my spare time, to chat you can reach me at <a href="mailto:raulfelixcarrizo@gmail.com">raulfelixcarrizo@gmail.com</a></p>
+        <NextButton target="engineer" color="#4A4A4A" />
       </div>
-    </section>
-  )
+    </div>
+  </section>
+)
 
 export default Current;
