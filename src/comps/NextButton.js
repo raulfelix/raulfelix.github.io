b95d8a3c1eb838e45ea44fe1e7778b4c4c0f75d1
raul <raul.carrizo@inquisitive.com>
2021-09-29 18:06:20 +1000

    next button style update

--- src/comps/NextButton.js
+++ src/comps/NextButton.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { scrollBodyTo } from '../util/Scroller';
 
+import * as Styled from './next-button.styles';
+
 const NextButton = ({target, color = '#000'}) => (
   <div className="next" style={{textAlign: 'center'}}>
-    <button
+    <Styled.NextButton
       type="button"
       className="btn-next hvr-wobble-vertical"
       style={{color}}
       onClick={() => scrollBodyTo(document.getElementById(target))}
       >
         <i className="icon-chevron-down" />
-    </button>
+    </Styled.NextButton>
   </div>
 )
 
---
+++ src/comps/next-button.styles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import { linkText } from "./colour.styles";
+
+export const NextButton = styled.button`
+  background: none;
+  font-size: 24px;
+  padding: 28px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:focus {
+    box-shadow: inset 0 0 0 3px ${linkText};
+    outline: none;
+  }
+`;
