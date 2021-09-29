312f9e95a24e60f2d49d9acd7b01465dad331b8e
raul <raul.carrizo@inquisitive.com>
2021-09-29 23:10:20 +1000

    remove sass

--- src/comps/footer.styles.tsx
+++ src/comps/footer.styles.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
+import { rem } from "./utils.styles";
+
 export const Footer = styled.footer`
   background-color: #b762ff;
-  padding: rem(100) 0;
+  padding: ${rem(100)} 0;
   display: flex;
   align-items: center;
   justify-content: center;
