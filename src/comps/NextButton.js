2ca7d7aa5e630d983831c74cc129909a2756c010
raul <raul.carrizo@inquisitive.com>
2021-09-29 23:20:22 +1000

    Add dropdown icon

--- src/comps/NextButton.js
+++ src/comps/NextButton.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { scrollBodyTo } from '../util/Scroller';
 
+import iconDropDown from '../images/icon_dropdown.svg';
+
 import * as Styled from './next-button.styles';
 
 const NextButton = ({target, color = '#000'}) => (
   <div className="next" style={{textAlign: 'center'}}>
     <Styled.NextButton
       type="button"
-      className="btn-next hvr-wobble-vertical"
+      className="hvr-wobble-vertical"
       style={{color}}
       onClick={() => scrollBodyTo(document.getElementById(target))}
       >
-        <i className="icon-chevron-down" />
+        <img src={iconDropDown} alt="" />
     </Styled.NextButton>
   </div>
 )
