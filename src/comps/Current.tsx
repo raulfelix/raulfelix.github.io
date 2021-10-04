8c6102d0f40e2e9764d0060a27708d0103e74a8e
raul <raul.carrizo@inquisitive.com>
2021-10-04 19:25:43 +1100

    Add current supporting graphic

--- src/comps/Current.tsx
+++ src/comps/Current.tsx
@@ -1,37 +1,58 @@
 import React from "react";
 
-import { Row } from "./Global.styles";
-import NextButton from "./next-button";
 import { Header50, Link, Text } from "./typography.styles";
 import * as Styled from "./current.styles";
+import NextButton from "./next-button";
+import inqLogo from "../images/inquisitive_logo.svg";
 
 const Current = () => (
   <Styled.Current id="current">
-    <Row>
-      <Header50>Currently...</Header50>
-      <Text>
-        I am working with{" "}
-        <Link
-          href="https://www.inquisitive.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Inquisitive
-        </Link>{" "}
-        where we are building a platform to bring quality educational content to
-        every person on Earth.
-      </Text>
-      <Text>
-        I like to collaborate in my spare time, to chat you can reach me via{" "}
-        <Link
-          href="https://www.linkedin.com/in/ra%C3%BAl-felix-carrizo-70716439/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </Link>
-      </Text>
-    </Row>
+    <Styled.CurrentRow>
+      <Styled.CurrentCol>
+        <Header50>Currently...</Header50>
+        <Text>
+          I’m working with{" "}
+          <Link
+            href="https://inquisitive.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Inquisitive
+          </Link>{" "}
+          where we are building a platform to bring quality educational content
+          to every person on Earth.
+        </Text>
+        <Text>
+          I like to collaborate in my spare time, to chat you can reach me via{" "}
+          <Link
+            href="https://www.linkedin.com/in/ra%C3%BAl-felix-carrizo-70716439/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </Link>
+        </Text>
+      </Styled.CurrentCol>
+      <Styled.CurrentCol>
+        <Styled.CurrentPreview>
+          <Styled.PreviewFrame>
+            <Styled.PreviewFrameHeader>
+              <img src={inqLogo} alt="" />
+            </Styled.PreviewFrameHeader>
+            <Styled.PreviewFrameContent>
+              <Styled.PreviewFrameRow>
+                <Styled.PreviewFrameCard isHighlight />
+                <Styled.PreviewFrameCard />
+              </Styled.PreviewFrameRow>
+              <Styled.PreviewFrameRowCutOff>
+                <Styled.PreviewFrameCard />
+                <Styled.PreviewFrameCard />
+              </Styled.PreviewFrameRowCutOff>
+            </Styled.PreviewFrameContent>
+          </Styled.PreviewFrame>
+        </Styled.CurrentPreview>
+      </Styled.CurrentCol>
+    </Styled.CurrentRow>
     <NextButton target="engineer" />
   </Styled.Current>
 );
--- src/comps/current.styles.tsx
+++ src/comps/current.styles.tsx
@@ -1,5 +1,123 @@
 import styled from "styled-components";
 
+import { Row } from "./Global.styles";
+import { rem } from "./utils.styles";
+
 export const Current = styled.div`
   background-color: #f9f0ff;
 `;
+
+export const CurrentRow = styled(Row)`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+export const CurrentCol = styled.div`
+  width: 100%;
+  @media (min-width: 968px) {
+    width: 40%;
+    &:first-child {
+      padding-right: ${rem(60)};
+      width: 60%;
+    }
+  }
+`;
+
+export const CurrentPreview = styled.div`
+  background-color: #722ed1;
+  border: 3px solid #000;
+  border-radius: ${rem(16)} 0 0 0;
+  max-width: 560px;
+  margin: auto;
+  padding-top: ${rem(24)};
+  padding-left: ${rem(24)};
+`;
+
+export const PreviewFrame = styled.div`
+  background-color: #ffffff;
+  border-radius: ${rem(8)} 0 0 0;
+`;
+
+export const PreviewFrameHeader = styled.div`
+  border-bottom: 1px solid #e3e3e3;
+  display: flex;
+  align-items: center;
+  padding-left: ${rem(32)};
+  height: ${rem(80)};
+
+  img {
+    height: ${rem(30)};
+  }
+`;
+
+export const PreviewFrameContent = styled.div`
+  padding: ${rem(24)} 0 0 ${rem(32)};
+`;
+
+export const PreviewFrameRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: ${rem(16)};
+`;
+
+type PreviewCardFrameProps = {
+  isHighlight?: boolean;
+};
+export const PreviewFrameCard = styled.div<PreviewCardFrameProps>`
+  height: ${rem(74)};
+
+  &:before {
+    content: "";
+    background-color: #f3f2f2;
+    border-radius: ${rem(8)};
+    display: block;
+    height: 100%;
+  }
+
+  &:first-child {
+    padding-right: ${rem(12)};
+    width: 60%;
+  }
+
+  &:last-child {
+    padding-left: ${rem(12)};
+    width: 40%;
+    &:before {
+      border-radius: ${rem(8)} 0 0 ${rem(8)};
+    }
+  }
+
+  ${({ isHighlight }) =>
+    isHighlight &&
+    `
+    &:before {
+      background-color: #D3ADF7;
+    }
+  `}
+`;
+
+export const PreviewFrameRowCutOff = styled(PreviewFrameRow)`
+  margin-bottom: 0;
+  ${PreviewFrameCard} {
+    height: ${rem(40)};
+
+    &:first-child:before {
+      border-radius: ${rem(8)} ${rem(8)} 0 0;
+    }
+
+    &:last-child:before {
+      border-radius: ${rem(8)} 0 0 0;
+    }
+  }
+`;
+
+export const purple1 = "#F9F0FF";
+export const purple2 = "#EFDBFF";
+export const purple3 = "#D3ADF7";
+export const purple5 = "#9254DE";
+export const purple6 = "#722ED1";
+export const purple7 = "#531DAB";
+export const purple8 = "#391085";
+export const purple9 = "#22075E";
+export const purple10 = "#120338";
