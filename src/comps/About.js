636dbfe6885467416bb466d185cd8f779b6f78bd
raul <raul.carrizo@inquisitive.com>
2026-03-20 22:20:33 +1100

    Typo

--- src/comps/About.js
+++ src/comps/About.js
@@ -7,7 +7,7 @@ const About = () => (
   <Styled.About id="about">
     <Row>
       <Header50>Rapid Fire</Header50>
-      <Text>The last book I read was 40,000 Weeks by Oliver Burkeman.</Text>
+      <Text>The last book I read was 4000 Weeks by Oliver Burkeman.</Text>
       <Text>We can chat in Spanish or Italian 🤌</Text>
       <Text>The machines are coming... 🤖</Text>
       <Text></Text>
